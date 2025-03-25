5a1ea681dae6d175e1a7861bf6ee9219e11c66a7
Movelocity <2814771302@qq.com>
2025-03-25 20:41:38 +0800

    feat: 调整左侧标签切换布局，包含选项: chat, agent, plugin, settings

--- apps/client/src/pages/Layout/components/LeftSider/index.tsx
+++ apps/client/src/pages/Layout/components/LeftSider/index.tsx
@@ -2,48 +2,80 @@ import ChatMenu from "@/pages/Chat/components/ChatMenu";
 import PluginsMenu from "@/pages/Plugins/components/Menu";
 import { Tabs, TabsProps } from "antd";
 import { createStyles } from "antd-style";
-import { VscExtensions, VscOctoface, VscSettings } from "react-icons/vsc";
+import { VscExtensions, VscOctoface, VscComment, VscSettings, VscSettingsGear  } from "react-icons/vsc";
 
 const useStyle = createStyles(({ token, css }) => {
   return {
     leftContent: css`
     `,
     tabs: css`
-        .ant-tabs-nav {
-            margin-left: 32px;
-            margin-bottom: 0;
-        }
+      .ant-tabs-nav {
+        padding: 0 16px;
+        margin-bottom: 0;
+      }
+      .ant-tabs-nav-list {
+        justify-content: center;
+        gap: 8px;
+      }
+      .ant-tabs-tab {
+        padding: 0;
+      }
+      .ant-tabs-tab-btn {
+        padding: 0 8px;
+      }
+      .ant-tabs-tab+.ant-tabs-tab {
+        margin: 0;
+      }
     `,
   };
 });
 
 const moreProps: TabsProps['more'] = {
-    icon: <VscSettings />,
-    trigger: 'click',
-    arrow: true,
+  icon: <VscSettings />,
+  trigger: 'click',
+  arrow: true,
+}
+
+const TabLabel = ({ icon }: { icon: React.ReactNode }) => {
+  return (
+    <div className="py-2">
+      {icon}
+    </div>
+  )
 }
 
 const LeftSider: React.FC = () => {
-    const { styles } = useStyle();
+  const { styles } = useStyle();
 
-    const tabItems: TabsProps['items'] = [
-        {
-            key: 'chat',
-            label: <VscOctoface fontSize={20} className={`mx-2`}/>,
-            children: <ChatMenu />
-        },
-        {
-            key: 'plugin',
-            label: <VscExtensions fontSize={20} className={`mx-2`}/>,
-            children: <PluginsMenu />
-        },
-    ]
+  const tabItems: TabsProps['items'] = [
+    {
+      key: 'chat',
+      label: <TabLabel icon={<VscComment fontSize={16}/>} />,
+      children: <ChatMenu />
+    },
+    {
+      key: 'agent',
+      label: <TabLabel icon={<VscOctoface fontSize={16}/>} />,
+      children: <div>Agents</div>
+    },
+    {
+      key: 'plugin',
+      label: <TabLabel icon={<VscExtensions fontSize={16}/>} />,
+      children: <PluginsMenu />
+    },
+    {
+      key: 'settings',
+      label: <TabLabel icon={<VscSettingsGear fontSize={16}/>} />,
+      children: <div>Settings</div>
+    },
+  ]
 
-    // 由于扩展功能还没上线，所以先隐藏
-    return <div className={styles.leftContent}>
-        {/* <Tabs items={tabItems} className={styles.tabs} more={moreProps} /> */}
-        <ChatMenu />
+  // 由于扩展功能还没上线，所以先隐藏
+  return (
+    <div className={styles.leftContent}>
+      <Tabs items={tabItems} className={styles.tabs} more={moreProps} />
     </div>
+  )
 }
 
 export default LeftSider;
