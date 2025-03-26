34befc658a1751dd6fd6e609522b6f1ae70033b6
FamousMai <famousmai@163.com>
2025-03-26 17:15:27 +0800

    feat(web): 修改底部信息

--- apps/admin/web/src/components/Footer/index.tsx
+++ apps/admin/web/src/components/Footer/index.tsx
@@ -8,24 +8,24 @@ const Footer: React.FC = () => {
       style={{
         background: 'none',
       }}
-      copyright="Powered by Ant Desgin"
+      copyright="Powered by Gaia-X"
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'Gaia-X-Admin',
+          title: 'Gaia-X-Admin',
+          href: 'https://github.com/YFGaia/gaia-x',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/YFGaia/gaia-x',
           blankTarget: true,
         },
         {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          key: 'Gaia-X',
+          title: 'Gaia-X',
+          href: 'https://github.com/YFGaia/gaia-x',
           blankTarget: true,
         },
       ]}
