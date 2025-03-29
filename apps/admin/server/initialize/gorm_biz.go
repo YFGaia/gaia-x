96c95f8caa1d6b7b5b92829382060fea5838418b
FamousMai <famousmai@163.com>
2025-03-29 19:18:08 +0800

    fix: 修改名称

--- apps/admin/server/initialize/gorm_biz.go
+++ apps/admin/server/initialize/gorm_biz.go
@@ -2,11 +2,14 @@ package initialize
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/gaia_x"
 )
 
 func bizModel() error {
 	db := global.GVA_DB
-	err := db.AutoMigrate()
+	err := db.AutoMigrate(
+		gaia_x.Version{},
+	)
 	if err != nil {
 		return err
 	}
