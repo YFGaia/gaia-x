777fc3fcce9dee9d60e7f822e55b96c19efe6c16
Ace <934740389@qq.com>
2025-03-29 14:19:29 +0800

    fix(einox): 去除多余注释说明

--- apps/admin/server/third_party/einox/llmadapter.go
+++ apps/admin/server/third_party/einox/llmadapter.go
@@ -1,19 +1,3 @@
-/*
- * Copyright 2024 CloudWeGo Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // Package einox provides a unified adapter interface for Large Language Models (LLMs)
 // 软件包einox提供了大型语言模型（LLMS）的统一适配器接口
 package einox
