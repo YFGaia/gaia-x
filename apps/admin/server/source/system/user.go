eb39dd9c6596d6a7abca7727d12a923af3b6c51d
FamousMai <famousmai@163.com>
2025-03-29 17:59:03 +0800

    fix: 初始化数据，管理名名称修改

--- apps/admin/server/source/system/user.go
+++ apps/admin/server/source/system/user.go
@@ -2,6 +2,7 @@ package system
 
 import (
 	"context"
+
 	sysModel "github.com/flipped-aurora/gin-vue-admin/server/model/system"
 	"github.com/flipped-aurora/gin-vue-admin/server/service/system"
 	"github.com/flipped-aurora/gin-vue-admin/server/utils"
@@ -59,7 +60,7 @@ func (i *initUser) InitializeData(ctx context.Context) (next context.Context, er
 			UUID:        uuid.New(),
 			Username:    "admin",
 			Password:    adminPassword,
-			NickName:    "Mr.奇淼",
+			NickName:    "管理员",
 			HeaderImg:   "https://qmplusimg.henrongyi.top/gva_header.jpg",
 			AuthorityId: 888,
 			Phone:       "17611111111",
@@ -69,7 +70,7 @@ func (i *initUser) InitializeData(ctx context.Context) (next context.Context, er
 			UUID:        uuid.New(),
 			Username:    "a303176530",
 			Password:    password,
-			NickName:    "用户1",
+			NickName:    "普通用户1",
 			HeaderImg:   "https:///qmplusimg.henrongyi.top/1572075907logo.png",
 			AuthorityId: 9528,
 			Phone:       "17611111111",
