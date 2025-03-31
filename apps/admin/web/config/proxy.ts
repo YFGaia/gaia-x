233025eba06b787f2e5be7ae6617ed9bf6db84b1
FamousMai <famousmai@163.com>
2025-03-31 17:20:16 +0800

    ci: admin-web端口统一改成8080

--- apps/admin/web/config/proxy.ts
+++ apps/admin/web/config/proxy.ts
@@ -12,7 +12,7 @@
 export default {
   // 如果需要自定义本地开发服务器  请取消注释按需调整
   dev: {
-    // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
+    // localhost:8080/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       // 要代理的地址
       target: 'http://localhost:8888',
@@ -28,7 +28,7 @@ export default {
    * @doc https://github.com/chimurai/http-proxy-middleware
    */
   test: {
-    // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
+    // localhost:8080/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       target: 'https://proapi.azurewebsites.net',
       changeOrigin: true,
