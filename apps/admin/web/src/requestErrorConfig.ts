1f6ac26403a4bd8803ad67dd5a203d07be7dfcbb
FamousMai <famousmai@163.com>
2025-04-09 09:32:36 +0800

    feat(admin): 请求拦截器，去掉默认带上token

--- apps/admin/web/src/requestErrorConfig.ts
+++ apps/admin/web/src/requestErrorConfig.ts
@@ -109,7 +109,8 @@ export const errorConfig: RequestConfig = {
   requestInterceptors: [
     (config: RequestOptions) => {
       // 拦截请求配置，进行个性化处理。
-      const url = config?.url?.concat('?token = 123');
+      const url = config?.url;
+      // const url = config?.url?.concat('?token = 123');
       return { ...config, url };
     },
   ],
